6f8f5e7996b5d26cc9cd811b6986e3eeacd49b2a
Kang Hye Jin <34128604+kanghyejin0106@users.noreply.github.com>
2021-04-06 22:16:38 +0900

    button setting
    -toast
    -naver page
    -call number

--- app/src/main/java/android/develop/hello/MainActivity.java
+++ app/src/main/java/android/develop/hello/MainActivity.java
@@ -1,8 +1,12 @@
 package android.develop.hello;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickMainButton(View v){
+        Toast.makeText(this, "pushed", Toast.LENGTH_LONG).show();
+    }
+    public void onClickNaverButton(View v){
+        Intent naverIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(naverIntent);
+    }
+    public void onClickCallButton(View v){
+        Intent callIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1234-1313"));
+        startActivity(callIntent);
     }
 }
