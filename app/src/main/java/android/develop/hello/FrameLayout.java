58ca1e266436751aefacd96b52c56d06045e25ad
Kang Hye Jin <34128604+kanghyejin0106@users.noreply.github.com>
2021-04-12 20:49:29 +0900

    layout
    linear layout, frame layout, imageView, change Main Activity

---
+++ app/src/main/java/android/develop/hello/FrameLayout.java
@@ -0,0 +1,41 @@
+package android.develop.hello;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FrameLayout extends AppCompatActivity {
+
+    ImageView imageView;
+    ImageView imageView2;
+    int imageIndex = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_frame_layout);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView2 = (ImageView)findViewById(R.id.imageView2);
+
+    }
+
+    public void onButton1Clicked(View v){
+        changeImage();
+    }
+
+    private void changeImage(){
+        if(imageIndex==0){
+            imageView.setVisibility(View.VISIBLE);
+            imageView2.setVisibility(View.INVISIBLE);
+            imageIndex=1;
+        }else{
+            imageView.setVisibility(View.INVISIBLE);
+            imageView2.setVisibility(View.VISIBLE);
+            imageIndex=0;
+        }
+    }
+
+}
