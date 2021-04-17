6ff0c5a477d87bbae68f4253c53e1cfb29884b86
Kang Hye Jin <34128604+kanghyejin0106@users.noreply.github.com>
2021-04-17 17:09:57 +0900

    new Intent(Activity)
    display new Activity with new Intent.
    and android theme.
    and result, request code

--- app/src/main/java/android/develop/hello/MainActivity3.java
+++ app/src/main/java/android/develop/hello/MainActivity3.java
@@ -1,17 +1,36 @@
 package android.develop.hello;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity3 extends AppCompatActivity {
+    private static final int REQUEST_CODE_MENU = 101;
     LinearLayout container;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_CODE_MENU){
+            Toast.makeText(getApplicationContext(), "호출됨", Toast.LENGTH_LONG).show();
+
+            if(resultCode == RESULT_OK){
+                String name = data.getStringExtra("name");
+                Toast.makeText(getApplicationContext(), "호출받은 name: "+name , Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +47,15 @@ public class MainActivity3 extends AppCompatActivity {
                 CheckBox checkBox = container.findViewById(R.id.checkBox2);
                 checkBox.setText("로딩완료");
 
+            }
+        });
+
+        Button button1 = findViewById(R.id.button5);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity4.class);
+                startActivityForResult(intent, REQUEST_CODE_MENU);
             }
         });
     }
---
+++ app/src/main/java/android/develop/hello/MainActivity4.java
@@ -0,0 +1,28 @@
+package android.develop.hello;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+
+        Button button = findViewById(R.id.button6);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("name", "mike");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+}
